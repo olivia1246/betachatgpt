b26e1052a450843fadf5227139b20bb3d493baf7
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2024-10-04 01:29:32 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -9,7 +9,6 @@ HTML_TEMPLATE = '''
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://unpkg.com/7.css" />
     <title>ChatGPT 1.0</title>
 </head>
 <body>
